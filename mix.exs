4b0eea20dc45b98f31ca00ec18b61269cfed8751
Corey Powell <mistdragon100@gmail.com>
2024-07-26 11:51:20 -0500

    Bumped version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule UnitFmt.MixProject do
   def project do
     [
       app: :unit_fmt,
-      version: "0.1.0",
+      version: "0.2.0",
       description: description(),
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
